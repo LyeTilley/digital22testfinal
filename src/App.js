9d940c39d7f70cd495603a2506dfbb300c2220fe
LyeTilley <74318846+LyeTilley@users.noreply.github.com>
2021-08-07 12:15:20 -0300

    first

--- src/App.js
+++ src/App.js
@@ -11,45 +11,128 @@ class App extends Component {
     super();
 
     this.state={
-      search:null
+      CLIENT_ID:'371805',
+      CLIENT_SECRET:'2753c706-1502-4c22-a1be-41349c49ab38',
+      search:null,
+      firstname: undefined,
+      lastname: undefined,
+      province: undefined,
+      email: undefined,
     };
   }
 
-  render(){
-    const styleInfo = {
-      paddingRight:'10px'
+  assignRegion = (province) => {
+    switch(province) {
+      case 'BC':
+        return "Pacific";
+      case 'AB':
+      case 'SK':
+      case 'MB':
+        return "Prairie";
+      case 'ON':
+      case 'QC':
+        return "Eastern";
+      case 'NB':
+      case 'NS':
+      case 'PE':
+      case 'NL':
+        return "Atlantic";
+      default:
+        return "NA";
     }
-    const elementStyle ={
-      border:'solid',
-      borderRadius:'6px',
-      position:'relative',
-      left:'10vh',
-      height:'3vh',
-      width:'20vh',
-      marginTop:'5vh',
-      marginBottom:'10vh'
-    }
-    const items = Information.map(data=>{
-      return(
-      <div>
-        <ul>
-          <li style={{position:'relative',left:'10vh'}}>
-            <span style={styleInfo}>{data.name}</span>
-            <span style={styleInfo}>{data.age}</span>
-            <span style={styleInfo}>{data.province}</span>
-            <span style={styleInfo}>{data.region}</span>
-          </li>
-        </ul>
-      </div>
-      )
-    })
+  }
 
+  onSubmitForm = (event) => {
+    const xhr = new XMLHttpRequest();
+    const url = 'https://api.hsforms.com/submissions/v3/integration/submit/20536262/f72dc414-cb6a-4c20-a167-0a937c43a925';
+
+    const data = {
+      fields: [
+        {
+          name: "email",
+          value: this.state.email
+        },
+        {
+          name: "firstname",
+          value: this.state.firstname
+        },
+        {
+          name: "lastname",
+          value: this.state.lastname
+        },
+        {
+          name: "state",
+          value: this.state.province
+        },
+        {
+          name: "country",
+          value: this.assignRegion(this.state.province),
+        }
+      ],
+      "legalConsentOptions":{ // Include this object when GDPR options are enabled
+        "consent":{
+          "consentToProcess":true,
+          "text":"I agree to allow Example Company to store and process my personal data.",
+          "communications":[
+            {
+              "value":true,
+              "subscriptionTypeId":999,
+              "text":"I agree to receive marketing communications from Example Company."
+            }
+          ]
+        }
+      }
+    };
+
+    xhr.open('POST', url);
+    // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4 && xhr.status == 200) { 
+            alert(xhr.responseText); // Returns a 200 response if the submission is successful.
+        } else if (xhr.readyState == 4 && xhr.status == 400){ 
+            alert(xhr.responseText); // Returns a 400 error the submission is rejected.          
+        } else if (xhr.readyState == 4 && xhr.status == 403){ 
+            alert(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.           
+        } else if (xhr.readyState == 4 && xhr.status == 404){ 
+            alert(xhr.responseText); //Returns a 404 error if the formGuid isn't found     
+        }
+       }
+
+
+    // Sends the request 
+    
+    xhr.send(JSON.stringify(data))
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+  }
+
+  render() {
     return (
       <div>
-      <input type="text" placeholder="Please add name and region here" style={elementStyle} onChange={(e)=>this.searchSpace(e)} />
-      {items}
+        <form onSubmit={this.onSubmitForm}>
+        <label>
+            Email:
+            <input type="text" name="email" onChange={(e) => { this.setState({name: e.target.value})}} />
+          </label>
+          <label>
+            First Name:
+            <input type="text" name="firstname" onChange={(e) => { this.setState({name: e.target.value})}} />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="lastname" onChange={(e) => { this.setState({name: e.target.value})}} />
+          </label>
+          <label>
+            Province:
+            <input type="text" name="province" onChange={(e) => { this.setState({province: e.target.value})}} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-    )
+    );
   }
 }
 
